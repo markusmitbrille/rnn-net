90f007f6195a474e671a1e6facab78a1d2e9e54f
Markus Rezai <markus.rezai@gmail.com>
2018-02-07 13:24:32 +0100

    Implemented simulation serialization

--- RNN.NET/Simulation.cs
+++ RNN.NET/Simulation.cs
@@ -1,7 +1,10 @@
 ﻿using Autrage.LEX.NET.Extensions;
+using Autrage.LEX.NET.Serialization;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using static Autrage.LEX.NET.DebugUtils;
 
 namespace Autrage.RNN.NET
 {
@@ -30,6 +33,70 @@ namespace Autrage.RNN.NET
         #endregion Properties
 
         #region Methods
+
+        public static Simulation Deserialize(Stream stream, params Serializer[] serializers)
+        {
+            Serializer[] networkSerializers = new Serializer[]
+            {
+                new Neuron.Serializer(),
+                new Muscle.Serializer(),
+                new Sensor.Serializer(),
+                new NeuralLayer.Serializer(),
+                new NeuralNetwork.Serializer(),
+                new NeuralNetwork.Genome.Serializer(),
+                new NeuralNetwork.Dud.Serializer(),
+                new NeuralNetwork.SigmonCreator.Serializer(),
+                new NeuralNetwork.PerceptronCreator.Serializer(),
+                new NeuralNetwork.NodeLinker.Serializer(),
+                new NeuralNetwork.InLinker.Serializer(),
+                new NeuralNetwork.OutLinker.Serializer(),
+            };
+
+            Marshaller marshaller = new Marshaller(serializers.Concat(networkSerializers).ToArray());
+
+            if (stream.ReadInt() is int networkCount)
+            {
+                Simulation simulation = new Simulation();
+                for (int i = 0; i < networkCount; i++)
+                {
+                    simulation.Add(marshaller.Deserialize<NeuralNetwork>(stream));
+                }
+
+                return simulation;
+            }
+            else
+            {
+                Warning("Could not read simulation network count!");
+                return null;
+            }
+        }
+
+        public void Serialize(Stream stream, params Serializer[] serializers)
+        {
+            Serializer[] networkSerializers = new Serializer[]
+            {
+                new Neuron.Serializer(),
+                new Muscle.Serializer(),
+                new Sensor.Serializer(),
+                new NeuralLayer.Serializer(),
+                new NeuralNetwork.Serializer(),
+                new NeuralNetwork.Genome.Serializer(),
+                new NeuralNetwork.Dud.Serializer(),
+                new NeuralNetwork.SigmonCreator.Serializer(),
+                new NeuralNetwork.PerceptronCreator.Serializer(),
+                new NeuralNetwork.NodeLinker.Serializer(),
+                new NeuralNetwork.InLinker.Serializer(),
+                new NeuralNetwork.OutLinker.Serializer(),
+            };
+
+            Marshaller marshaller = new Marshaller(serializers.Concat(networkSerializers).ToArray());
+
+            stream.Write(Count);
+            foreach (NeuralNetwork network in this)
+            {
+                marshaller.Serialize(stream, network);
+            }
+        }
 
         public void Genesis()
         {
