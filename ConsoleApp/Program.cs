a65e4855567b1a64925cff0d72768cd22b495f4f
Markus Rezai <markus.rezai@gmail.com>
2018-02-12 08:36:26 +0100

    Profiled solution
    Instrumentation must be chosen - CPU (and memory) sampling causes the PC to crash, due to security patches related to Meltdown/Spectre

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,15 @@
 ﻿using Autrage.LEX.NET.Extensions;
 using Autrage.RNN.NET;
+using Microsoft.CSharp;
 using System;
+using System.CodeDom.Compiler;
 using System.Collections;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using static System.Console;
 
 namespace ConsoleApp
 {
@@ -15,44 +20,49 @@ namespace ConsoleApp
         private static void Main(string[] args)
         {
             TestSimulation();
-
-            Console.ReadLine();
         }
+
+        private static void P_Died(object sender, EventArgs e) => WriteLine("method");
 
         private static void TestSimulation()
         {
             Simulation simulation = new Simulation()
             {
-                Order = 10,
-                Complexity = 100,
+                Order = 100,
+                Complexity = 1000,
             };
 
             Stopwatch stopwatch = Stopwatch.StartNew();
             simulation.Populate();
             stopwatch.Stop();
-            Console.WriteLine($"Genesis: {stopwatch.ElapsedMilliseconds}ms");
+            WriteLine($"Genesis: {stopwatch.ElapsedMilliseconds}ms");
 
             using (MemoryStream stream = new MemoryStream())
             {
                 stopwatch.Restart();
                 simulation.Serialize(stream);
                 stopwatch.Stop();
-                Console.WriteLine($"Serialization: {stopwatch.ElapsedMilliseconds}ms");
+                WriteLine($"Serialization: {stopwatch.ElapsedMilliseconds}ms");
 
                 stream.Reset();
 
                 stopwatch.Restart();
                 simulation = Simulation.Deserialize(stream);
                 stopwatch.Stop();
-                Console.WriteLine($"Deserialization: {stopwatch.ElapsedMilliseconds}ms");
-
-                stopwatch.Restart();
-                simulation.Serialize(stream);
-                stopwatch.Stop();
-                Console.WriteLine($"Serialization: {stopwatch.ElapsedMilliseconds}ms");
+                WriteLine($"Deserialization: {stopwatch.ElapsedMilliseconds}ms");
             }
         }
 
         #endregion Methods
+    }
+
+    class Person
+    {
+        public event EventHandler Died;
+
+        private void Die()
+        {
+            Died?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
